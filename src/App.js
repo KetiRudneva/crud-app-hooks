5f67d507549b704b794cb2f9adcc913efe454bbb
Keti <rudnevaketi@gmail.com>
2020-12-17 19:08:48 +0300

    Static data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import UserTable from './Components/UserTable'
 
 function App() {
+  	// Data
+	const usersData = [
+		{ id: 1, name: 'Кети', username: 'Сванетти' },
+		{ id: 2, name: 'Craig', username: 'List' },
+		{ id: 3, name: 'Ben', username: 'Uncle' },
+  ]
+
+  const [ users, setUsers ] = useState(usersData)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>CRUD App with Hooks</h1>
+      <div className="flex-large">
+					<h2>View users</h2>
+					<UserTable users={users} />
+				</div>
     </div>
+    
   );
 }
 
